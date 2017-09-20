a26ad1ef2f7bf237c4e9e4fe1bce0a87351e7b28
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-20 15:01:34 -0500

    implement basic page template

---
+++ page.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<?php while (have_posts()): the_post();
+  get_template_part('template-parts/content', 'page');
+endwhile; ?>
+
+<?php get_footer(); ?>
---
+++ template-parts/content-page.php
@@ -0,0 +1,6 @@
+<article id="post-<?php the_ID(); ?>">
+  <header>
+    <?php the_title(); ?>
+  </header>
+  <?php the_content(); ?>
+</article>

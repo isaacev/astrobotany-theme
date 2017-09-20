34b913694125e45c09c954f93b1d2eb055db1cb6
ievavold <ievavold@wisc.edu>
2017-09-20 12:52:40 -0500

    add header navigation skeleton

--- header.php
+++ header.php
@@ -8,6 +8,11 @@
     <header>
       <nav>
         <h1>astrobotany</h1>
+        <ul id="menu">
+          <li><a href="#">blog</a>
+          <li><a href="#">research</a>
+          <li><a href="#">shop</a>
+        </ul>
       </nav>
     </header>
     <main>

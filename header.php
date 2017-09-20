1ba132d1bde3e72b8be97429f04031773ae9f777
ievavold <ievavold@wisc.edu>
2017-09-20 11:41:31 -0500

    add skeleton header and footer

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <h1>astrobotany</h1>
+      </nav>
+    </header>
+    <main>

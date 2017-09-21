ae0aaf524da64136d31d65bd2b63a3edbb78c710
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-21 01:15:41 -0500

    add a basic pagination tempalte

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ if (have_posts()) {
   while (have_posts()) { the_post();
     get_template_part('template-parts/content', get_post_format());
   }
+
+  get_template_part('template-parts/util-pager');
 }
 
 get_footer();
---
+++ template-parts/util-pager.php
@@ -0,0 +1,4 @@
+<nav class="pagination">
+  <?php next_posts_link('Older posts'); ?>
+  <?php previous_posts_link('Newer posts'); ?>
+</nav>

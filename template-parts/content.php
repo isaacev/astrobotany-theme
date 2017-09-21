95aaccce0153eda7b55c12858d424f3e40bf1ecb
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-21 01:15:12 -0500

    add default content template

---
+++ template-parts/content.php
@@ -0,0 +1,8 @@
+<article>
+  <header>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <p><?php the_date(); ?></p>
+    <p><?php the_author(); ?></p>
+  </header>
+  <?php the_excerpt(); ?>
+</article>

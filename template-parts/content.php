280e2d26d20028c3a063b0dde60022a2f0808539
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-21 01:42:22 -0500

    use 'the_time()' intead of 'the_date()'

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,7 +1,7 @@
 <article>
   <header>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <p><?php the_date(); ?></p>
+    <p><?php the_time(); ?></p>
     <p><?php the_author(); ?></p>
   </header>
   <?php the_excerpt(); ?>

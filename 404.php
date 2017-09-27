e037c46b65bc612cfa1f463823c5252cc35317f3
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-27 16:07:52 -0500

    add basic 404 page

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<article>
+  <h1>404</h1>
+  <p>It seems that this page does not exist. It could be the result of a broken link or a mistyped URL.</p>
+</article>
+
+<?php get_footer(); ?>

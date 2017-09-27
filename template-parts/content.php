cfdffa670e12a8e2f588977b548e8ab5d8bd68cf
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-27 14:42:38 -0500

    change post title header tag based on single/archive view

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,9 +1,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <article>
-    <h1><?php the_title(); ?></h1>
     <?php if (is_single()): ?>
+      <h1><?php the_title(); ?></h1>
       <?php the_content(); ?>
     <?php else: ?>
+      <h2><?php the_title(); ?></h2>
       <?php the_excerpt(); ?>
       <p><a href="<?php the_permalink(); ?>">Read more</a></p>
     <?php endif; ?>

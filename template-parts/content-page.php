2b2c73c7a73e1b6c83966ad4dff1e1eea8b877b4
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-28 13:40:34 -0500

    build new page template

--- template-parts/content-page.php
+++
@@ -1,6 +0,0 @@
-<article id="post-<?php the_ID(); ?>">
-  <header>
-    <?php the_title('<h1>', '</h1>'); ?>
-  </header>
-  <?php the_content(); ?>
-</article>
---
+++ template-parts/content/page.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This template renders the content for an entire page.
+ */
+
+?>
+
+<?php page_header([
+  'header' => get_the_title(),
+]); ?>
+<article id="page-<?php the_ID(); ?>">
+  <?php the_content(); ?>
+</article>

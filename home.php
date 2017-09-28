9a94d37d3e3db1f0adfa1bd5de3ffe930410d160
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-28 13:30:23 -0500

    dedicated template for blog posts page

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+
+get_header();
+page_header([
+  'header'=> get_the_title(get_option('page_for_posts', true)),
+]);
+
+if (have_posts()) {
+  while (have_posts()) { the_post();
+    get_template_part('template-parts/content/post', get_post_format());
+  }
+
+  get_template_part('template-parts/util-pager');
+}
+
+get_footer();

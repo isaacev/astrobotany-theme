f28799f19d53d2fad9ef2577b54810673521f03b
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-28 23:37:45 -0500

    delegate search results rendering template within 'search.php'

--- search.php
+++ search.php
@@ -5,7 +5,13 @@ get_page_header('&lsquo;' . get_search_query() . '&rsquo;', 'Search results for'
 
 if (have_posts()) {
   while (have_posts()) { the_post();
-    get_template_part('template-parts/content/post', get_post_format());
+    if (get_post_type() === 'post') {
+      get_template_part('template-parts/content/post', get_post_format());
+    } else if (get_post_type() === 'page') {
+      get_template_part('template-parts/content/page');
+    } else {
+      get_template_part('template-parts/content/excerpt');
+    }
   }
 
   get_template_part('template-parts/part/pagination');

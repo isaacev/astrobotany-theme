d367502cf59d9b2e367d35b5c6b8a9b3274e62bd
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-28 22:37:59 -0500

    add linked title in non-single view

--- template-parts/content/post.php
+++ template-parts/content/post.php
@@ -25,6 +25,7 @@
     <?php if (is_single()): ?>
       <?php the_content(); ?>
     <?php else: ?>
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       <?php the_excerpt(); ?>
     <?php endif; ?>
   </article>

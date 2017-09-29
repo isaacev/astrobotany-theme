c4241eef7a3456e64ce47d80f89696bcd5e1f805
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-29 15:28:00 -0500

    add "Read more" link below excerpts

--- template-parts/content/post.php
+++ template-parts/content/post.php
@@ -28,6 +28,7 @@
     <?php else: ?>
       <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       <?php the_excerpt(); ?>
+      <p><a href="<?php the_permalink(); ?>">Read more</a></p>
     <?php endif; ?>
   </article>
   <aside>

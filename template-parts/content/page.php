c352623cad9b8e741bc2fec6db3a9da46cd0061b
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-28 23:35:41 -0500

    support page excerpts

--- template-parts/content/page.php
+++ template-parts/content/page.php
@@ -6,14 +6,27 @@
 
 ?>
 
-<?php page_header([
-  'header' => get_the_title(),
-]); ?>
+<?php if (is_page()): ?>
+  <?php page_header([
+    'header' => get_the_title(),
+  ]); ?>
 
-<article id="page-<?php the_ID(); ?>">
-  <?php if (get_the_content() === ''): ?>
-    <?php get_template_part('template-parts/part/empty'); ?>
-  <?php else: ?>
-    <?php the_content(); ?>
-  <?php endif; ?>
-</article>
+  <article id="page-<?php the_ID(); ?>">
+    <?php if (get_the_content() === ''): ?>
+      <?php get_template_part('template-parts/part/empty'); ?>
+    <?php else: ?>
+      <?php the_content(); ?>
+    <?php endif; ?>
+  </article>
+<?php else: ?>
+  <article id="page-<?php the_ID(); ?>">
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <article>
+      <?php if (get_the_content() === ''): ?>
+        <?php get_template_part('template-parts/part/empty'); ?>
+      <?php else: ?>
+        <?php the_excerpt(); ?>
+      <?php endif; ?>
+    </article>
+  </article>
+<?php endif; ?>

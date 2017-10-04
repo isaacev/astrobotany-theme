ab528594f5b9dad2cbcbe83384041e03a1b8ea31
Isaac Evavold <isaacev@users.noreply.github.com>
2017-10-04 17:20:50 -0500

    add default social media preview thumbnail

--- header.php
+++ header.php
@@ -3,6 +3,10 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/default.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1024">
+    <meta property="og:image:height" content="1024">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
   </head>

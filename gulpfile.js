b2d6e9db174a01dd73dd29c9e3369f4a07a78f76
Isaac Evavold <isaacev@users.noreply.github.com>
2017-10-05 17:43:58 -0500

    add 'bump' task to increment semver patch number

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,46 @@
+const fs   = require('fs')
 const gulp = require('gulp')
 const zip  = require('gulp-zip')
 const sass = require('gulp-sass')
 const maps = require('gulp-sourcemaps')
+const sem  = require('semver')
 const pkg  = require('./package.json')
+
+function replace (oldVer, newVer) {
+  return (filename) => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        const bumped = data.replace(oldVer, newVer)
+
+        fs.writeFile(filename, bumped, (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        })
+      })
+    })
+  }
+}
+
+gulp.task('bump', () => {
+  const oldVer = pkg.version
+  const newVer = sem.inc(oldVer, 'patch')
+
+  const inputs = [
+    './style.css',
+    './package.json',
+    './functions.php',
+  ]
+
+  return Promise.all(inputs.map(replace(oldVer, newVer)))
+})
 
 gulp.task('zip', () => {
   const include = [

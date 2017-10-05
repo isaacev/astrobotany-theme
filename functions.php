3f7bc3fcd103eae02d450370357d948480ad2d98
Isaac Evavold <isaacev@users.noreply.github.com>
2017-10-05 13:53:21 -0500

    bump from 1.0.3 -> 1.0.4

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ function enqueue_styles () {
   $handle = 'main';
   $src    = get_template_directory_uri() . '/styles/css/main.css';
   $deps   = array('font-open-sans');
-  $ver    = 'v1.0.3';
+  $ver    = 'v1.0.4';
   wp_enqueue_style($handle, $src, $deps, $ver);
 }
 

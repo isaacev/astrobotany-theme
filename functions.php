918faedbe9e6c16a0374280971ef40c5bb8217ef
Isaac Evavold <isaacev@users.noreply.github.com>
2017-10-05 14:02:34 -0500

    fix typo

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ function enqueue_styles () {
   $handle = 'main';
   $src    = get_template_directory_uri() . '/styles/css/main.css';
   $deps   = array('font-open-sans');
-  $ver    = 'v1.0.5';
+  $ver    = 'v1.0.6';
   wp_enqueue_style($handle, $src, $deps, $ver);
 }
 

a59d3a7db0539997618b9e563ea57029081fd7d0
Isaac Evavold <isaacev@users.noreply.github.com>
2017-10-05 00:36:47 -0500

    add gulp 'css' and 'css:watch' tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp')
 const zip  = require('gulp-zip')
+const sass = require('gulp-sass')
+const maps = require('gulp-sourcemaps')
 
 gulp.task('bundle', () => {
   const include = [
@@ -16,4 +18,19 @@ gulp.task('bundle', () => {
   gulp.src(include.concat(exclude.map(p => '!' + p)))
       .pipe(zip('astrobotany-theme.zip'))
       .pipe(gulp.dest('.'))
+})
+
+gulp.task('css', () => {
+  const input  = './styles/scss/main.scss'
+  const output = './styles/css'
+  gulp.src(input)
+      .pipe(maps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(maps.write('.'))
+      .pipe(gulp.dest(output))
+})
+
+gulp.task('css:watch', [ 'css' ], () => {
+  const input = [ './styles/scss/**/*.scss' ]
+  gulp.watch(input, [ 'css' ])
 })

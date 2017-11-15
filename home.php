0d6589d3a2065e30ae8d5660e3fad1723e6f08a7
isaacev <isaacevavold@gmail.com>
2017-11-15 16:32:25 -0600

    add featured posts to blog

--- home.php
+++ home.php
@@ -5,9 +5,16 @@ page_header([
   'header'=> get_the_title(get_option('page_for_posts', true)),
 ]);
 
+$is_first_sticky_post = true;
+
 if (have_posts()) {
   while (have_posts()) { the_post();
-    get_template_part('template-parts/content/post', get_post_format());
+    if (is_sticky() && $is_first_sticky_post) {
+      $is_first_sticky_post = false;
+      get_template_part('template-parts/content/featured', get_post_format());
+    } else {
+      get_template_part('template-parts/content/post', get_post_format());
+    }
   }
 
   get_template_part('template-parts/part/pagination');
---
+++ template-parts/content/featured.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This template is rendered for the first "sticky" post in the blog archive.
+ */
+
+?>
+
+<?php if (is_single()): ?>
+  <?php get_template_part('template-parts/content/post', get_post_format()); ?>
+<?php else: ?>
+  <article id="post-<?php the_ID(); ?>" <?php post_class('featured-post'); ?>>
+    <article class="copy">
+      <?php if (has_post_thumbnail( $post->ID ) ): ?>
+        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+        <a href="<?php the_permalink(); ?>" class="featured-image" style="background-image:url('<?php echo $image[0]; ?>');"></a>
+      <?php endif; ?>
+      <h3><em>Featured:</em></h3>
+      <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+      <p><?php print_author_name(); ?> &mdash; <em><?php print_post_date(); ?></em></p>
+    </article>
+  </article>
+  <hr>
+<?php endif; ?>

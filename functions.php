70d71165162b9cd05f5eac8258c05f7967e2cedf
isaacev <isaacevavold@gmail.com>
2017-12-04 17:44:33 -0600

    bump v1.0.33 -> v1.0.34

--- functions.php
+++ functions.php
@@ -115,7 +115,7 @@ function enqueue_styles () {
   $handle = 'main';
   $src    = get_template_directory_uri() . '/styles/css/main.css';
   $deps   = array('font-open-sans');
-  $ver    = '1.0.33';
+  $ver    = '1.0.34';
   wp_enqueue_style($handle, $src, $deps, $ver);
 }
 
